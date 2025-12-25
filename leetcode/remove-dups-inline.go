3042d241fded387e0de5080d343f9178405c23cd
Joshua Bradley <joshuabradley012@gmail.com>
2025-12-24 21:49:33 -0800

    finished remove dups py, improved go

--- leetcode/remove-dups-inline.go
+++ leetcode/remove-dups-inline.go
@@ -2,19 +2,25 @@ package main
 
 import "fmt"
 
-func removeDups(nums []int) {
-  w := 1
-  for r := 1; r < len(nums); r++{
-    if nums[r] != nums[r - 1] {
-      nums[w] = nums[r]
-      w++
-    }
-  }
+func removeDups(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	write := 1
+
+	for read := 1; read < len(nums); read++ {
+		if nums[read] != nums[read-1] {
+			nums[write] = nums[read]
+			write++
+		}
+	}
+
+	return write
 }
 
 func main() {
-  dups := []int{1, 1, 2, 2, 2, 4, 4, 5, 5, 5, 7, 7, 8}
-  removeDups(dups)
-  fmt.Println(dups)
-
+	dups := []int{1, 1, 2, 2, 2, 4, 4, 5, 5, 5, 7, 7, 8}
+	k := removeDups(dups)
+	fmt.Println(k, dups[:k])
 }
--- leetcode/remove-dups-inline.py
+++ leetcode/remove-dups-inline.py
@@ -1,8 +1,17 @@
 def removeDups(nums):
-    w = 1
-    r = 1
-    for n in nums:
+    if not nums:
+        return 0
+
+    write = 1
+
+    for read in range(1, len(nums)):
+        if nums[read] != nums[read - 1]:
+            nums[write] = nums[read]
+            write += 1
+
+    return write
+
 
 nums = [1, 1, 2, 2, 2, 4, 4, 4, 4, 5, 5, 5, 6, 6, 8, 8]
-removeDups(nums)
-println(nums)
+k = removeDups(nums)
+print(k, nums[:k])
